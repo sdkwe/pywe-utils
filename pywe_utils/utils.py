83a60e8c7b2fd37ecca7146dfefa76984522f102
Brightcells <brightcells@gmail.com>
2016-11-21 11:04:22 +0800

    Comment reason of verify=False

--- pywe_utils/utils.py
+++ pywe_utils/utils.py
@@ -9,6 +9,8 @@ class WechatUtils(object):
         self.OPEN_DOMAIN = 'https://open.weixin.qq.com'
 
     def get(self, url, verify=False, **kwargs):
+        # When ``verify=True`` and ``cacert.pem`` not match ``https://xxx.weixin.qq.com``, will raise
+        # SSLError: [Errno 1] _ssl.c:510: error:14090086:SSL routines:SSL3_GET_SERVER_CERTIFICATE:certificate verify failed
         res = requests.get(url.format(**kwargs), verify=verify)
         res.encoding = 'utf-8'
         return res.json()
